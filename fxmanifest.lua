d59829a5e2a570f685bc099cb39dfdf4d19e701b
aezakmi15799 <56330122+aezakmi15799@users.noreply.github.com>
2023-11-05 04:06:53 +0700

    Update fxmanifest.lua
    Signed-off-by: aezakmi15799 <56330122+aezakmi15799@users.noreply.github.com>

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -15,7 +15,7 @@ fx_version 'cerulean'
 game 'gta5'
 author '<Discord : AlertX>, <Website : Not >'
 description 'This resource is created by BABYSHOP\'s Scripts'
-version '1.1.1'
+version '1.0.8'
 
 shared_scripts {
 	'config/general.setting.lua',
